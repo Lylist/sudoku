e7d8902d7c5838a503fb2c1a1348a6caa636109f
Lylist <1303404574@qq.com>
2018-04-09 00:01:06 +0800

    生成函数2.0
    接入工程

--- generator.cpp
+++ generator.cpp
@@ -1,30 +1,27 @@
-#include<iostream>
-#include<stdlib.h>
-#include<time.h> 
-#include <cstdio>
-using namespace std;
+#include "sudoku.h"
 int Map[9][9];	//数独地图
 int TransMap[9][9];  //用于变换的数独
 int Raw[9][9];	//记录第i行出现的1-9
 int Col[9][9];	//记录第j列出现的1-9
-int B[9][9];	/*第i个九宫出现的1-9*/
+int Palace[9][9];	/*第i个九宫出现的1-9*/
 int n;			//记录迭代次数
-bool newsudu(int x);		//x 开始的位置，距离0的位置
-void clearrandom(int* r);
-int newrandom(int* r);
-void coutsudu(int Sudoku_Map[][9]);
-void Transform(int Col1, int Col2, int Raw1, int Raw2, int Number1, int Number2);
+bool New_Sudoku(int x);		//x 开始的位置，距离0的位置
+void Clear_Random(int* r);   //清空随机数组
+int Generator_Random(int* r);     //随机生成
+void Print_Map(int Sudoku_Map[][9]); //输出终局
+void Transform(int Col1, int Col2, int Raw1, int Raw2, int Number1, int Number2);   //通过变换生成新的终局
 int main()
 {
-	freopen("xx.txt", "w", stdout);
+	freopen("sudoku.txt", "w", stdout);
+	/*计时器
 	clock_t startTime, endTime;
-	startTime = clock();
+	startTime = clock();*/
 	srand((int)time(0));
 	int t = 1000000;
 	int num = 0;
 	while (t)
 	{
-		if (num %1373 == 0)
+		if (num %1373 == 0)               //一个种子终局可以产生1373个变换终局
 		{
 			for (int i = 0; i < 9; i++)
 			{
@@ -33,21 +30,21 @@ int main()
 					Map[i][j] = 0;
 					Raw[i][j] = 0;
 					Col[i][j] = 0;
-					B[i][j] = 0;
+					Palace[i][j] = 0;
 				}
 			}
 			n = 1;
 			Map[0][0] = 1;
 			Raw[0][0] = 1;
 			Col[0][0] = 1;
-			B[0][0] = 1;
-			newsudu(1);
-			coutsudu(Map);
+			Palace[0][0] = 1;
+			New_Sudoku(1);
+			Print_Map(Map);
 			num++;
 			t--;
 			continue;
 		}
-		for (int Col1 = 1; Col1 < 9; Col1++)
+		for (int Col1 = 1; Col1 < 9; Col1++)   //先变换列，在变换行，再交换数字
 		{
 			if (t == 0) break;
 			int UP_Col = Col1 / 3 * 3 + 2;
@@ -70,7 +67,6 @@ int main()
 								Transform(Col1, Col2, Raw1, Raw2, Number1, Number2);
 								num++;
 								t--;
-								cout << num << endl;
 							}
 						}
 					}
@@ -78,50 +74,39 @@ int main()
 			}
 		}
 	}
+	/*计时器
 	endTime = clock();
-	cout << "Totle Time : " << (double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
-	cout << num << endl;
-	//cout<<n<<endl;
-	//getchar();
+	cout << "Totle Time : " << (double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;*/
 	return 0;
 }
-bool newsudu(int x)	//x 开始的位置，距离0的位置
+bool New_Sudoku(int x)	//x为当前的位置
 {
-	if (x == 81)
-	{
-		return true;
-	}
-	int r[9];		//用来随即生成1-9，且不能重复
-	clearrandom(r);
+	if (x == 81) return true;
+	int RandomRaw[9];		//用来随即生成1-9
+	Clear_Random(RandomRaw);
 	//开始往Map[i][j]插数据
 	do
 	{
-		int num = newrandom(r);
-		if (num == 0)	//不行了
-		{
-			//cout<<x<<endl;
-			return false;
-		}
-		if (Raw[x / 9][num - 1] == 0 && Col[x % 9][num - 1] == 0 && B[((x / 9) / 3) * 3 + (x % 9 / 3)][num - 1] == 0)
+		int num = Generator_Random(RandomRaw);
+		//出现矛盾
+		if (num == 0) return false;
+		if (Raw[x / 9][num - 1] == 0 && Col[x % 9][num - 1] == 0 && Palace[((x / 9) / 3) * 3 + (x % 9 / 3)][num - 1] == 0)
 		{
 			Raw[x / 9][num - 1] = 1;
 			Col[x % 9][num - 1] = 1;
-			B[((x / 9) / 3) * 3 + (x % 9 / 3)][num - 1] = 1;
+			Palace[((x / 9) / 3) * 3 + (x % 9 / 3)][num - 1] = 1;
 			Map[x / 9][x % 9] = num;
 			n++;
-			//coutsudu();
-			if (newsudu(x + 1) == true)
-			{
-				return true;
-			}
+			if (New_Sudoku(x + 1) == 1) return 1;
+			//回溯
 			Map[x / 9][x % 9] = 0;
 			Raw[x / 9][num - 1] = 0;
 			Col[x % 9][num - 1] = 0;
-			B[((x / 9) / 3) * 3 + (x % 9 / 3)][num - 1] = 0;
+			Palace[((x / 9) / 3) * 3 + (x % 9 / 3)][num - 1] = 0;
 		}
 	} while (1);
 }
-void coutsudu(int Sudoku_Map[][9])
+void Print_Map(int Sudoku_Map[][9])
 {
 	for (int i = 0; i<9; i++)
 	{
@@ -130,12 +115,11 @@ void coutsudu(int Sudoku_Map[][9])
 			printf("%d%c", Sudoku_Map[i][j], j == 8 ? '\n' : ' ');
 		}
 	}
-	//cout<<"---------------------------------"<<endl;
 	printf("\n");
 }
 void Transform(int Col1, int Col2, int Raw1, int Raw2, int Number1, int Number2)
 {
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < 9; i++)    //交换数字
 	{
 		for (int j = 0; j < 9; j++)
 		{
@@ -144,78 +128,35 @@ void Transform(int Col1, int Col2, int Raw1, int Raw2, int Number1, int Number2)
 			else TransMap[i][j] = Map[i][j];
 		}
 	}
+	//交换列
 	for (int i = 0; i < 9; i++) swap(TransMap[i][Col1], TransMap[i][Col2]);
+	//交换行
 	for (int i = 0; i < 9; i++) swap(TransMap[Raw1][i], TransMap[Raw2][i]);
-	coutsudu(TransMap);
+	Print_Map(TransMap);
 }
-void TransformNumber(int Number1, int Number2)
+void Clear_Random(int* RandomRaw)
 {
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{	
-			if (Map[i][j] == Number1) TransMap[i][j] = Number2;
-			else if (Map[i][j] == Number2) TransMap[i][j] = Number1;
-			else TransMap[i][j] = Map[i][j];
-		}
-	}
-	coutsudu(TransMap);
+	for (int i = 0; i<9; i++) RandomRaw[i] = 0;
 }
-void TransformCol(int Col1, int Col2)
-{
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			if (j == Col1) TransMap[i][j] = Map[i][Col2];
-			else if (j == Col2) TransMap[i][j] = Map[i][Col1];
-			else TransMap[i][j] = Map[i][j];
-		}
-	}
-	coutsudu(TransMap);
-}
-void TransformRaw(int Raw1, int Raw2)
-{
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			if (i == Raw1) TransMap[i][j] = Map[Raw2][j];
-			else if (i == Raw2) TransMap[i][j] = Map[Raw1][j];
-			else TransMap[i][j] = Map[i][j];
-		}
-	}
-	coutsudu(TransMap);
-}
-void clearrandom(int* r)
-{
-	for (int i = 0; i<9; i++)
-	{
-		r[i] = 0;
-	}
-}
-int newrandom(int* r)	//1-9
+int Generator_Random(int* RandomRaw)	//1-9随机生成
 {
 	int num;
-	do
+	num = rand() % 9;
+	if (RandomRaw[num] == 0)
 	{
-		num = rand() % 9;
-		if (r[num] == 0)
+		RandomRaw[num] = 1;
+		return num + 1;
+	}
+	else
+	{
+		for (int i = 0; i < 9; i++)
 		{
-			r[num] = 1;
-			return num + 1;
-		}
-		else
-		{
-			for (int i = 0; i<9; i++)
+			if (RandomRaw[i] == 0)
 			{
-				if (r[i] == 0)
-				{
-					r[i] = 1;
-					return i + 1;
-				}
+				RandomRaw[i] = 1;
+				return i + 1;
 			}
-			return 0;
 		}
-	} while (1);
+		return 0;
+	}
 }
